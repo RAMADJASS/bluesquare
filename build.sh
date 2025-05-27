e2255c92112b86a240a006cd91b051249afc6395
RAMADJASS <ramadanadjassa248@gmail.com>
2025-05-27 10:17:36 +0100

    Ajout de fichiers pour Render

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+composer install --no-dev --optimize-autoloader
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+mkdir -p database
+touch database/database.sqlite
+
+php artisan migrate --force
